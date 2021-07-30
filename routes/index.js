b11cd55ddd4d8d357961a2a685f7fbadda309680
Akhil-EM <akhilemcalicut@gmail.com>
2021-07-30 11:31:59 +0530

    new api added

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var {listUsers,deleteUser}=require('../util/sql-query')
-/* GET home page. */
+var { listUsers, deleteUser } = require('../util/sql-query')
+    /* GET home page. */
 router.get('/', function(req, res, next) {
-                listUsers('')
-                     .then((_data)=>{
-                          console.log(_data)
-                          res.render('index', {status:'success',data:_data,title:'student list'});
-                     }).catch((error)=>{
-                          console.log('error');
-                          res.render('index', {status:'error',data:{message:error},title:'student list'});
-                     })
-              
+    listUsers('')
+        .then((_data) => {
+            console.log(_data)
+            res.render('index', { status: 'success', data: _data, title: 'student list' });
+        }).catch((error) => {
+            console.log('error');
+            res.render('index', { status: 'error', data: { message: error }, title: 'student list' });
+        })
+
 });
+
 // delete
-router.post('/:id',(req,res)=>{
-      var userId=req.params.id;
-      deleteUser(userId)
-           .then(()=>{
-              res.redirect('/');
-           }).catch(()=>{
-               console.log('error');
-           })
+router.post('/:id', (req, res) => {
+    var userId = req.params.id;
+    deleteUser(userId)
+        .then(() => {
+            res.redirect('/');
+        }).catch(() => {
+            console.log('error');
+        })
 })
 
 //edit
 
+router.get('/user-count', (req, res) => {
+    listUsers('')
+        .then((_data) => {
+            res.json({
+                userCount: (_data).length
+            })
+        }).catch((error) => {
+            res.json({
+                userCount: 0
+            })
+        })
+})
 
 
 module.exports = router;
